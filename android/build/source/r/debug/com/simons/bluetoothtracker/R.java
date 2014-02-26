ff0968f4899e1b963dfbb64983b61585f4db38ac
Gerard Simons <G.J.C.Simons@student.tudelft.nl>
2014-02-26 16:14:00 +0100

    rotating menu activity added

--- android/build/source/r/debug/com/simons/bluetoothtracker/R.java
+++ android/build/source/r/debug/com/simons/bluetoothtracker/R.java
@@ -11,30 +11,45 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bike=0x7f020000;
+        public static final int briefcase=0x7f020001;
+        public static final int cloud=0x7f020002;
+        public static final int compass=0x7f020003;
+        public static final int designer_bag=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int key=0x7f020006;
+        public static final int logo=0x7f020007;
+        public static final int map=0x7f020008;
+        public static final int umbrella=0x7f020009;
     }
     public static final class id {
-        public static final int btRefreshRateSettings=0x7f06000b;
-        public static final int calibrationLimitSettings=0x7f060008;
+        public static final int btRefreshRateSettings=0x7f060011;
+        public static final int buttonBar=0x7f06000a;
+        public static final int calibrationLimitSettings=0x7f06000e;
+        public static final int cloud_button=0x7f06000d;
         public static final int compassView=0x7f060005;
+        public static final int compass_button=0x7f06000b;
         public static final int connection_state=0x7f060001;
         public static final int deltaRssiValues=0x7f060004;
         public static final int device_address=0x7f060000;
         public static final int device_name=0x7f060007;
         public static final int device_rssi=0x7f060006;
-        public static final int fragmentsNumberSettings=0x7f060009;
-        public static final int maxValuesSizeSettings=0x7f06000a;
-        public static final int menu_connect=0x7f060012;
-        public static final int menu_disconnect=0x7f060013;
-        public static final int menu_refresh=0x7f060010;
-        public static final int menu_scan=0x7f060014;
-        public static final int menu_settings=0x7f060011;
-        public static final int menu_stop=0x7f060015;
-        public static final int pointerWidthSettings=0x7f06000c;
+        public static final int fragmentsNumberSettings=0x7f06000f;
+        public static final int logo=0x7f060009;
+        public static final int map_button=0x7f06000c;
+        public static final int maxValuesSizeSettings=0x7f060010;
+        public static final int menuRootView=0x7f060008;
+        public static final int menu_connect=0x7f060018;
+        public static final int menu_disconnect=0x7f060019;
+        public static final int menu_refresh=0x7f060016;
+        public static final int menu_scan=0x7f06001a;
+        public static final int menu_settings=0x7f060017;
+        public static final int menu_stop=0x7f06001b;
+        public static final int pointerWidthSettings=0x7f060012;
         public static final int rssi_values=0x7f060002;
-        public static final int showColorsSwitch=0x7f06000d;
-        public static final int showDebugTextSwtich=0x7f06000f;
-        public static final int showPointerSwitch=0x7f06000e;
+        public static final int showColorsSwitch=0x7f060013;
+        public static final int showDebugTextSwtich=0x7f060015;
+        public static final int showPointerSwitch=0x7f060014;
         public static final int timeDeltaValues=0x7f060003;
     }
     public static final class layout {
@@ -43,7 +58,8 @@ public final class R {
         public static final int device_control_test=0x7f030002;
         public static final int device_scan=0x7f030003;
         public static final int listitem_device=0x7f030004;
-        public static final int settings=0x7f030005;
+        public static final int menu_activity=0x7f030005;
+        public static final int settings=0x7f030006;
     }
     public static final class menu {
         public static final int gatt_services=0x7f050000;
--- android/src/com/simons/bluetoothtracker/activities/MenuActivity.java
+++ android/src/com/simons/bluetoothtracker/activities/MenuActivity.java
@@ -1,64 +1,332 @@
-package .........src.com.simons.bluetoothtracker.activities;
+package com.simons.bluetoothtracker.activities;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.simons.bluetoothtracker.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MenuActivity extends Activity {
+
+    private static final String TAG = "MenuActivity";
+
+    private boolean relayoutMenu = true;
+
+    private int buttonSize;
+    private int distanceToCenter;
+
+    private ImageView logoView;
+    private float logoCenterX, logoCenterY;
+
+    private List<LinearLayout> rotatingButtons;
+    //The button that is at the hotspot (i.e. currently selected icon)
+    private Button hotspotButton;
+
+    private RelativeLayout rootView;
+
+    private float hotSpotAngle = 90F;
+
+    private List<Animator> runningAnimations;
+
+    //The logo rotation animation should only take place one, this boolean takes care of that
+    private boolean introAnimationsDone = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.menu_activity);
 
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
+        rotatingButtons = new ArrayList<LinearLayout>();
+        runningAnimations = new ArrayList<Animator>();
+
+        logoView = (ImageView) findViewById(R.id.logo);
+
+        rootView = (RelativeLayout) findViewById(R.id.menuRootView);
+        //Find the buttons
+        for (int i = 0; i < rootView.getChildCount(); i++) {
+            View child = rootView.getChildAt(i);
+            if (child instanceof LinearLayout) {
+                LinearLayout layout = (LinearLayout) child;
+                if(layout.getId() != R.id.buttonBar) { //Do not add the button bar
+                    rotatingButtons.add(layout);
+                }
+            }
         }
+        //Check when the layout has properly finished before doing anything UI related
+        rootView.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+            @Override
+            public void onLayoutChange(View view, int i, int i2, int i3, int i4, int i5, int i6, int i7, int i8) {
+                if(!introAnimationsDone) {
+                    doRotateLogoAnimation();
+                }
+                if (relayoutMenu) {
+                    int minDim = Math.min(rootView.getWidth(),rootView.getHeight());
+                    distanceToCenter = (int) (minDim / 2.5F);
+                    buttonSize = (int) (minDim / 5F);
+                    populateMenu(rotatingButtons);
+                }
+            }
+        });
+
+        rootView.setOnTouchListener(new View.OnTouchListener() {
+            //Origin coordinates
+            private float touchDownX,touchDownY;
+            //Direction of drag; either up or down, up being positive, down being negative
+            private int direction = -1;
+            //last coordinates
+            private float lastTouchY;
+            private int moveDistanceThreshold = 5;
+
+            //How often to update the position, 1 meaning every cycle
+            private final int motionThrottle = 1;
+            //Take track of the counts for the throttle purpose
+            private int eventCount = 0;
+
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+//                    Log.d(TAG, "motionEvent Down");
+                    touchDownX = motionEvent.getX();
+                    touchDownY = motionEvent.getY();
+                    lastTouchY = touchDownY;
+                } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+//                    Log.d(TAG, "motionEvent Up");
+                    LinearLayout button = buttonClosestToHotspot();
+                    float distance = angularDistanceToHotSpot(button);
+                    rotateButtonsAnimation(distance,750);
+//                    float distance = (float)Math.sqrt(Math.pow(motionEvent.getX() - touchDownX,2) + Math.pow(motionEvent.getY() - touchDownY,2));
+//                    setRotationButtons(distance / 10000F * 360F);
+                } else if (motionEvent.getAction() == MotionEvent.ACTION_MOVE) {
+//                    Log.d(TAG, "motionEvent Move");
+                    eventCount++;
+                    if (eventCount == motionThrottle) {
+                        eventCount = 0;
+                        if (motionEvent.getY() > lastTouchY + moveDistanceThreshold) {
+                            lastTouchY = motionEvent.getY();
+                            direction = -1;
+                        } else if (motionEvent.getY() < lastTouchY - moveDistanceThreshold) {
+                            lastTouchY = motionEvent.getY();
+                            direction = 1;
+                        } else {
+                            Log.d(TAG,"No direction.");
+                            return true;
+                        }
+                        float distance = (float) Math.sqrt(Math.pow(motionEvent.getX() - touchDownX, 2) + Math.pow(motionEvent.getY() - touchDownY, 2));
+                        rotateButtonsInstant(direction * distance / 100F);
+                    }
+                }
+                return true;
+            }
+        });
+
+        Button mapButton = (Button) findViewById(R.id.map_button);
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MenuActivity.this,"Not yet implemented.",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button cloudButton = (Button) findViewById(R.id.map_button);
+        cloudButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MenuActivity.this,"Not yet implemented.",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button compassButton = (Button) findViewById(R.id.compass_button);
+        compassButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MenuActivity.this,"Not yet implemented.",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
+    private float angularDistanceToHotSpot(LinearLayout rotatingButtonWrapper) {
+        return hotSpotAngle - rotatingButtonWrapper.getRotation();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+    private void populateMenu(List<LinearLayout> buttons) {
+
+        logoCenterX = Math.round(rootView.getWidth() / 2F + rootView.getX());
+        logoCenterY = Math.round(rootView.getHeight() / 2F + rootView.getY());
+
+        Log.d(TAG, "centerX = " + logoCenterX);
+        Log.d(TAG, "centerY = " + logoCenterY);
+
+        float angleRadians = 0F;
+        float angleDelta = (float) (Math.PI * 2 / buttons.size());
+
+        relayoutMenu = false;
+
+        logoView.setX(logoCenterX - logoView.getWidth() / 2);
+        logoView.setY(logoCenterY - logoView.getHeight() / 2);
+
+        for (int i = 0; i < buttons.size(); i++) {
+            final LinearLayout buttonWrapper = rotatingButtons.get(i);
+
+            Log.d(TAG, "angle (in Radians) = " + angleRadians);
+
+            //Set the position according to the circle around the logo
+            buttonWrapper.setX(logoCenterX + distanceToCenter - buttonSize / 2);
+            buttonWrapper.setY(logoCenterY - buttonSize / 2);
+
+            //Set the pivot point to be the center of the logo
+            buttonWrapper.setPivotX(logoCenterX - buttonWrapper.getX());
+            buttonWrapper.setPivotY(logoCenterY - buttonWrapper.getY());
+
+            float angleDegrees = (float) Math.toDegrees(angleRadians);
+
+            buttonWrapper.setRotation(angleDegrees);
+            final Button button = getButtonFromWrapper(buttonWrapper);
+            button.setRotation(-angleDegrees);
+
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    cancelAllAnimations();
+                    hotspotButton = button;
+                    rotateButtonsAnimation(angularDistanceToHotSpot(buttonWrapper),1500);
+                }
+            });
+
+            angleRadians += angleDelta;
+//            rootView.addView(button);
         }
-        return super.onOptionsItemSelected(item);
+//        rotateButtons(90F);
+        relayoutMenu = true;
     }
 
     /**
-     * A placeholder fragment containing a simple view.
+     * Returns the button wrapper that is closest to the hotspot location.
+     * @return the wrapper layout that contains the actual button.
      */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
+    private LinearLayout buttonClosestToHotspot() {
+        float bestRotation = -1F;
+        LinearLayout closestButton = null;
+        for (LinearLayout button : rotatingButtons) {
+            float distance = Math.abs(hotSpotAngle - button.getRotation());
+            if (closestButton == null || distance < bestRotation) {
+                bestRotation = distance;
+                closestButton = button;
+            }
         }
+        return closestButton;
+    }
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_menu, container, false);
-            return rootView;
+    private void placeButtonAroundLogo(LinearLayout buttonWrapper, float angleRadians) {
+
+    }
+
+    private Button getButtonFromWrapper(LinearLayout buttonWrapper) {
+        return (Button) buttonWrapper.getChildAt(0);
+    }
+
+    private void rotateButtonsInstant(float angleIncrement) {
+        for (final LinearLayout buttonWrapper : rotatingButtons) {
+            buttonWrapper.setRotation(buttonWrapper.getRotation() + angleIncrement);
+            Button button = getButtonFromWrapper(buttonWrapper);
+            button.setRotation(button.getRotation() - angleIncrement);
+        }
+    }
+
+    private void doRotateLogoAnimation() {
+        ObjectAnimator rotation = ObjectAnimator.ofFloat(logoView,"rotation",360F);
+//        ObjectAnimator rotationOvershoot = ObjectAnimator.ofFloat(logoView,"rotation",360F);
+
+        AnimatorSet allAnimations = new AnimatorSet();
+        allAnimations.playSequentially(rotation);
+
+        rotation.setInterpolator(new AccelerateDecelerateInterpolator());
+
+        allAnimations.setStartDelay(300);
+        allAnimations.setDuration(1400);
+        allAnimations.start();
+
+        introAnimationsDone = true;
+    }
+
+    private void cancelAllAnimations() {
+        for (Animator animator : runningAnimations) {
+            animator.cancel();
+        }
+        runningAnimations.clear();
+    }
+
+    private void rotateButtonsAnimation(final float angle, int duration) {
+        if (angle != 0F) {
+            cancelAllAnimations();
+            runningAnimations.clear();
+            for (final LinearLayout buttonWrapper : rotatingButtons) {
+
+                final Button button = getButtonFromWrapper(buttonWrapper);
+                float fromAngle = buttonWrapper.getRotation();
+
+                float toAngle = fromAngle + angle;
+//                button.incrementRotationAroundLogo(angle);
+
+//                Log.d(TAG, "ID = " + buttonWrapper.getId());
+//                Log.d(TAG, "Angle = " + angle);
+//                Log.d(TAG, "fromAngle = " + fromAngle);
+//                Log.d(TAG, "toAngle = " + toAngle);
+
+                //Epic somersaults, if you'd like
+                int somerSaults = 0;
+
+                ObjectAnimator wrapperRotation = ObjectAnimator.ofFloat(buttonWrapper, "rotation", toAngle);
+                ObjectAnimator buttonRotation = ObjectAnimator.ofFloat(button, "rotation", 360F * somerSaults + button.getRotation() - angle);
+
+                wrapperRotation.setDuration(duration);
+                buttonRotation.setDuration(duration);
+
+                wrapperRotation.addListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animator) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animator) {
+//                        Log.d(TAG, "Button wrapper rotation = " + buttonWrapper.getRotation());
+//                        Log.d(TAG, "Button rotation = " + button.getRotation());
+                        buttonWrapper.setRotation((buttonWrapper.getRotation() + 360F) % 360F);
+                        button.setRotation((button.getRotation() + 360F) % 360F);
+                        runningAnimations.clear();
+//                        Log.d(TAG, "Button wrapper normalized rotation = " + buttonWrapper.getRotation());
+//                        Log.d(TAG, "Button normalized rotation = " + button.getRotation());
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animator) {
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animator) {
+                    }
+                });
+
+                wrapperRotation.start();
+                buttonRotation.start();
+
+            }
+        } else {
+            Log.d(TAG, "Angle was 0; no rotation necessary.");
         }
     }
 
